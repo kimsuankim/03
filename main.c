bc6fdeacc19edb0550444fd43ccab391a736bfd8
kimsuankim <144988337+kimsuankim@users.noreply.github.com>
2023-09-21 16:10:51 +0900

    2

--- main.c
+++ main.c
@@ -4,16 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-    int input_int;
-    float input_float;
-    
-    printf("enter an integer : ");
-    scanf("%d", &input_int);
-    
-    printf("enter a float : ");
-    sacnf("%f" , &input_float);
-    
-    printf("integer : %d, float : %f¡¬n", input_int, input_float);
+   char c;
+   int i;
+   
+   printf("input a number :7");
+   scanf("%c", &c);
+   
+   i= c - '0';
+   printf("the input number ascii is %i\n", i);
+   printf("the input number is %i\n", i);
     
 	return 0;
 }
